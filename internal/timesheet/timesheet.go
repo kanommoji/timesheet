8da309ae15202e67fd81c75944b5b9985e22854a
kanommoji <uracha_p@kkumail.com>
2019-10-15 17:45:52 +0700

    [Added]: สร้างฟังก์ชัน CalculateNetWage ใน internal/timesheet/

--- internal/timesheet/timesheet.go
+++ internal/timesheet/timesheet.go
@@ -14,3 +14,17 @@ func (timesheet Timesheet) UpdateIncomeByID(year, month int, memberID string) er
 func CalculateNetSalary(salary, incomeTax1, socialSecurity float64) float64 {
 	return salary - incomeTax1 - socialSecurity
 }
+
+func CalculateNetWage(incomeTax53Percentag int, paymentWage, salary float64) float64 {
+	wage := CalculateWage(paymentWage, salary)
+	incomeTax53 := CalculateIncomeTax53(wage, incomeTax53Percentag)
+	return wage - incomeTax53
+}
+
+func CalculateWage(paymentWage, salary float64) float64 {
+	return 75000.00
+}
+
+func CalculateIncomeTax53(wage float64, incomeTax53Percentag int) float64 {
+	return 7500.00
+}
