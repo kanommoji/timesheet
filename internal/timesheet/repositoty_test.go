0e33267d035bb20bbc3257893e6c0e8b8af2a4ea
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-15 17:23:43 +0700

    [Created]: สร้างฟังก์ชันเทส func Test_UpdateIncomeByID_Input_Year_2018_Month_12_MemberID_001_Income_Should_Be_True ในไฟล์ repositoty_test.go

--- internal/timesheet/repositoty_test.go
+++ internal/timesheet/repositoty_test.go
@@ -50,3 +50,39 @@ func Test_GetSummary_Input_Year_2018_Month_17_Should_Be_TransactionTimesheet(t *
 	assert.Equal(t, nil, err)
 	assert.Equal(t, expected, actual)
 }
+
+func Test_UpdateIncomeByID_Input_Year_2018_Month_12_MemberID_001_Income_Should_Be_True(t *testing.T) {
+	expected := true
+
+	dBConnection, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/timesheet")
+	defer dBConnection.Close()
+
+	year := 2018
+	month := 12
+	memberID := "001"
+	incomes := []timesheet.Incomes{
+		{
+			Day:                      28,
+			StartTimeAM:              "09:00:00",
+			EndTimeAM:                "12:00:00",
+			StartTimePM:              "13:00:00",
+			EndTimePM:                "18:00:00",
+			Overtime:                 0,
+			TotalHours:               8,
+			CoachingCustomerCharging: 15000,
+			CoachingPaymentRate:      10000,
+			TrainingWage:             0,
+			OtherWage:                0,
+			Company:                  "siam_chamnankit",
+			Description:              "[KBTG] 2 Days Agile Project Management",
+		},
+	}
+
+	repository := timesheet.TimesheetRepository{
+		DBConnection: dBConnection,
+	}
+
+	actual := repository.UpdateIncomeByID(year, month, memberID, incomes)
+
+	assert.Equal(t, expected, actual)
+}
