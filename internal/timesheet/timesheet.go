68310988de3f53c13ba9db6616ae7db7e47d44d1
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-16 11:01:39 +0700

    [Created]: สร้างฟังก์ชัน CalculateTotalCoachingPaymentRate ในไฟล์ internal/timesheet/timesheet.go

--- internal/timesheet/timesheet.go
+++ internal/timesheet/timesheet.go
@@ -129,3 +129,24 @@ func CalculateTotalOtherWage(incomes []model.Incomes, company string) float64 {
 	}
 	return 0
 }
+
+func CalculateTotalCoachingPaymentRate(incomes []model.Incomes, company string) float64 {
+	totalCoachingPaymentRate := 0
+	if company == "siam_chamnankit" {
+		for i := range incomes {
+			if incomes[i].Company == "siam_chamnankit" {
+				totalCoachingPaymentRate += incomes[i].CoachingPaymentRate
+			}
+		}
+		return float64(totalCoachingPaymentRate)
+	}
+	if company == "shuhari" {
+		for i := range incomes {
+			if incomes[i].Company == "shuhari" {
+				totalCoachingPaymentRate += incomes[i].CoachingPaymentRate
+			}
+		}
+		return float64(totalCoachingPaymentRate)
+	}
+	return 0
+}
