e9112fd0a13516321df9f100047489653fe17ffd
kanommoji <uracha_p@kkumail.com>
2019-10-17 18:19:21 +0700

    [Added]: สร้างเทส Test_VerifyTimsheet_Input_Payment_MemberID_006_Year_2019_Month_11_Should_Be_Create_Timesheet ใน internal/repository/

--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -313,11 +313,63 @@ func Test_UpdateTransactionTimsheet_Input_Transaction_MemberID_001_Should_Be_No_
 	assert.Equal(t, nil, err)
 }
 
+func Test_VerifyTimsheet_Input_Payment_MemberID_006_Year_2019_Month_11_Should_Be_Create_Timesheet(t *testing.T) {
+	memberID := "006"
+	month := 2019
+	year := 11
+	payment := model.Payment{
+		TotalHoursHours:               120,
+		TotalHoursMinutes:             0,
+		TotalHoursSeconds:             0,
+		TotalCoachingCustomerCharging: 0.00,
+		TotalCoachingPaymentRate:      0.00,
+		TotalTrainigWage:              0.00,
+		TotalOtherWage:                0.00,
+		PaymentWage:                   0.00,
+	}
+
+	databaseConnection, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/timesheet")
+	defer databaseConnection.Close()
+	repository := TimesheetRepository{
+		DatabaseConnection: databaseConnection,
+	}
+
+	err := repository.VerifyTimesheet(payment, memberID, year, month)
+
+	assert.Equal(t, nil, err)
+}
+
+func Test_VerifyTimsheet_Input_Payment_MemberID_007_Year_2019_Month_11_Should_Be_Update_Timesheet(t *testing.T) {
+	memberID := "007"
+	month := 11
+	year := 2019
+	payment := model.Payment{
+		TotalHoursHours:               16,
+		TotalHoursMinutes:             0,
+		TotalHoursSeconds:             0,
+		TotalCoachingCustomerCharging: 15000.00,
+		TotalCoachingPaymentRate:      10000.00,
+		TotalTrainigWage:              10000.00,
+		TotalOtherWage:                0.00,
+		PaymentWage:                   20000.00,
+	}
+
+	databaseConnection, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/timesheet")
+	defer databaseConnection.Close()
+	repository := TimesheetRepository{
+		DatabaseConnection: databaseConnection,
+	}
+
+	err := repository.VerifyTimesheet(payment, memberID, year, month)
+
+	assert.Equal(t, nil, err)
+}
+
 func Test_CreateTimsheet_Input_Payment_MemberID_006_Should_Be_No_Error(t *testing.T) {
 	timesheetID := "006201912"
 	memberID := "006"
-	month := 2019
-	year := 12
+	month := 12
+	year := 2019
 	payment := model.Payment{
 		TotalHoursHours:               120,
 		TotalHoursMinutes:             0,
