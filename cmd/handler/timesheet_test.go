2d80e702bba0695a50770411b70b7374d9c71b6d
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-17 13:57:52 +0700

    [Edited]: แก้ไขเทส Test_CalculatePaymentHandler_Input_MemberID_001_Year_2018_Month_12_Should_Be_200 ในไฟล์ cmd/handler/timesheet_test.go

--- cmd/handler/timesheet_test.go
+++ cmd/handler/timesheet_test.go
@@ -244,7 +244,7 @@ func Test_CalculatePaymentHandler_Input_MemberID_001_Year_2018_Month_12_Should_B
 		PaymentWage:                   195000.00,
 	})
 
-	mockRepository.On("CreateTimesheet", mock.Anything).Return(nil)
+	mockRepository.On("CreateTimesheet", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
 
 	mockRepository.On("GetMemberByID", "001").Return([]model.Member{
 		{
