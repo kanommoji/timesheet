4717ba5334a5af041abcf93ac98adf477f5d68f7
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-18 18:06:08 +0700

    [Added]: สร้างไฟล์ config.go

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+	Username string `yamal: "username"`
+	Password string `yamal: "password"`
+	Host     string `yamal: "host"`
+	Port     string `yamal: "port"`
+	Database string `yamal: "database"`
+}
+
+func (databaseConfig DatabaseConfig) GetURI() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", databaseConfig.Username, databaseConfig.Password, databaseConfig.Host, databaseConfig.Port, databaseConfig.Database)
+}
+
+func SetupDatabaseConfig(databaseConfigPath string) (DatabaseConfig, error) {
+	var databaseConfig DatabaseConfig
+
+	databaseConfigFile, err := ioutil.ReadFile(databaseConfigPath)
+	if err != nil {
+		return databaseConfig, err
+	}
+
+	err = yaml.Unmarshal(databaseConfigFile, &databaseConfig)
+	if err != nil {
+		return databaseConfig, err
+	}
+
+	return databaseConfig, err
+}
