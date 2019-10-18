daff6eb7e6b52f73a403a61bb42707cad6ddc234
kanommoji <uracha_p@kkumail.com>
2019-10-18 11:29:54 +0700

    [Edited]: เพิ่ม pointer ใส่ function mock ใน cmd/mockapi/

--- cmd/mockapi/mock.go
+++ cmd/mockapi/mock.go
@@ -10,32 +10,32 @@ type MockRepository struct {
 	mock.Mock
 }
 
-func (mock MockRepository) GetSummary(year, month int) ([]model.TransactionTimesheet, error) {
+func (mock *MockRepository) GetSummary(year, month int) ([]model.TransactionTimesheet, error) {
 	argument := mock.Called(year, month)
 	return argument.Get(0).([]model.TransactionTimesheet), argument.Error(1)
 }
 
-func (mock MockRepository) CreateIncome(year, month int, memberID string, income model.Incomes) error {
+func (mock *MockRepository) CreateIncome(year, month int, memberID string, income model.Incomes) error {
 	argument := mock.Called(year, month, memberID, income)
 	return argument.Error(0)
 }
 
-func (mock MockRepository) GetIncomes(memberID string, year, month int) ([]model.Incomes, error) {
+func (mock *MockRepository) GetIncomes(memberID string, year, month int) ([]model.Incomes, error) {
 	argument := mock.Called(memberID, year, month)
 	return argument.Get(0).([]model.Incomes), argument.Error(1)
 }
 
-func (mock MockRepository) GetMemberByID(memberID string) ([]model.Member, error) {
+func (mock *MockRepository) GetMemberByID(memberID string) ([]model.Member, error) {
 	argument := mock.Called(memberID)
 	return argument.Get(0).([]model.Member), argument.Error(1)
 }
 
-func (mock MockRepository) VerifyTransactionTimsheet(transactionTimesheet []model.TransactionTimesheet) error {
+func (mock *MockRepository) VerifyTransactionTimsheet(transactionTimesheet []model.TransactionTimesheet) error {
 	argument := mock.Called(transactionTimesheet)
 	return argument.Error(0)
 }
 
-func (mock MockRepository) VerifyTimesheet(payment model.Payment, memberID string, year int, month int) error {
+func (mock *MockRepository) VerifyTimesheet(payment model.Payment, memberID string, year int, month int) error {
 	argument := mock.Called(payment, memberID, year, month)
 	return argument.Error(0)
 }
@@ -44,12 +44,12 @@ type MockTimesheet struct {
 	mock.Mock
 }
 
-func (mock MockTimesheet) CalculatePayment(incomes []model.Incomes) model.Payment {
+func (mock *MockTimesheet) CalculatePayment(incomes []model.Incomes) model.Payment {
 	argument := mock.Called(incomes)
 	return argument.Get(0).(model.Payment)
 }
 
-func (mock MockTimesheet) CalculatePaymentSummary(member []model.Member, incomes []model.Incomes, year, month int) []model.TransactionTimesheet {
+func (mock *MockTimesheet) CalculatePaymentSummary(member []model.Member, incomes []model.Incomes, year, month int) []model.TransactionTimesheet {
 	argument := mock.Called(member, incomes, year, month)
 	return argument.Get(0).([]model.TransactionTimesheet)
 }
