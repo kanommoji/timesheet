72624c333e95e9fc6a3a74587daa92d86ba0bf18
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-10-18 18:07:15 +0700

    [Edited]: แก้ไขฟังก์ชัน main ในไฟล์ cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 	"timesheet/cmd/handler"
+	"timesheet/config"
 	"timesheet/internal/repository"
 	"timesheet/internal/timesheet"
 
@@ -13,8 +15,16 @@ import (
 )
 
 func main() {
-	router := gin.Default()
-	databaseConnection, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/timesheet")
+	var databaseConfigPath string
+	flag.StringVar(&databaseConfigPath, "config", "../timesheet/config/database.yml", "Database config path")
+	flag.Parse()
+
+	databaseConfig, err := config.SetupDatabaseConfig(databaseConfigPath)
+	if err != nil {
+		log.Fatal("Cannot read config", err.Error())
+	}
+
+	databaseConnection, err := sql.Open("mysql", databaseConfig.GetURI())
 	if err != nil {
 		log.Fatal("Cannot connect database", err.Error())
 	}
@@ -26,6 +36,8 @@ func main() {
 			DatabaseConnection: databaseConnection,
 		},
 	}
+
+	router := gin.Default()
 
 	router.POST("/showSummaryTimesheet", api.GetSummaryHandler)
 	router.POST("/addIncomeItem", api.UpdateIncomeHandler)
